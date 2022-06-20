c0f45f248d3a33c032383d14c67f71719122824a
Private <msaleh.abdelrahman@gmail.com>
2022-06-20 19:33:38 +0200

    x_cars

---
+++ README.md
@@ -0,0 +1,13 @@
+# x-cars
+
+## start with 
+
+    ```expo start```
+
+from expo app on phone connect to the test server
+
+<hr/>
+
+![gif](Documentation/run.gif)
+
+![gif](Documentation/deleteAndEmpty.gif)
---
+++ constants/colors.js
@@ -0,0 +1,16 @@
+const colors = {
+    background: '#181a1b',
+    navBackground: '#17212f',
+    text: '#e8e6e3',
+    textShadow: 'rgba(0,0,0,0.75)',
+    primary: '#824bf5',
+    secondary: '#867979',
+    tertiary: '#3498db',
+    new: '#104b04',
+    used: '#1a457c',
+    error: '#910606',
+    active: '#824bf5',
+    inActive: '#f0edf6',
+}
+
+export default colors;
